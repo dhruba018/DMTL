9abbfe5841886b27264e1dd3f908d40955b93524
dhruba018 <dhruba018@gmail.com>
2020-12-10 02:50:16 -0600

    Updating files

--- R/RF_predict.R
+++ R/RF_predict.R
@@ -1,31 +1,31 @@
-## Perform predictive modeling using Random Forest regression 
-## 
-## Dependency: randomForest 
-## Dependency_own: lambda_functions 
+## Perform predictive modeling using Random Forest regression
+##
+## Dependency: randomForest
+## Dependency_own: lambda_functions
 ################################################################################
 
 RF_predict <- function(x_train, y_train, x_test, y_lims, n_tree = 300, m_try = 0.3333, random_seed = NULL, ...) {
-    
-    source("lambda_functions.R")
-    
+
+    # source("lambda_functions.R")
+
     ## Initital checks...
     # if (!require(randomForest))                 # Load package
     #     library(randomForest)
-    
+
     if (missing(y_lims))
         y_lims <- c(min(y_train), max(y_train))
-    
+
     if (m_try > 1 | m_try < 0)
         stop("Invalid value! Please choose a value between 0 and 1 (fraction of the features)!")
-    
-    
+
+
     ## Define model & perform prediction...
     set.seed(random_seed)                       # For reproducibility
-    
+
     m_try  <- round(m_try * ncol(x_train))
     Forest <- randomForest::randomForest(x = x_train, y = y_train, ntree = n_tree, mtry = m_try, replace = TRUE, ...)
-    
+
     y_pred <- confined(predict(Forest, x_test), lims = y_lims)
     y_pred
-    
+
 }
--- R/dist_match_trans_learn.R
+++ R/dist_match_trans_learn.R
@@ -1,5 +1,5 @@
-## Perform distribution mapping based transfer learning regression 
-## 
+## Perform distribution mapping based transfer learning regression
+##
 ## Dependency: stats, ks, randomForest
 ## Dependency_own: lambda_functions, get_dist_est, dist_match, RF_predict
 ##
@@ -7,52 +7,52 @@
 ################################################################################
 
 DMTL <- function(target_set, source_set, method = "hist", size = 1e3, seed = NULL, pred_all = FALSE) {
-    
+
     ## Initial check...
     if (ncol(target_set[["X"]]) != ncol(source_set[["X"]]))
         stop("Source and target set covariates must have the same number of features!")
-    
+
     if (nrow(source_set[["X"]]) != length(source_set[["y"]]))
         stop("Source sets must have the same number of samples!")
-    
+
     out01 <- function(y) (min(y) < 0) & (max(y) > 1)
     if (out01(target_set[["y"]]) | out01(source_set[["y"]]))
         stop("Response data must be normalized in [0, 1].")
-    
-    
+
+
     ## Load function files...
-    source("lambda_functions.R")
-    source("get_dist_est.R")
-    source("dist_match.R")
-    source("RF_predict.R")
-    
-    
+    # source("lambda_functions.R")
+    # source("get_dist_est.R")
+    # source("dist_match.R")
+    # source("RF_predict.R")
+
+
     ######## MAIN ##############################################################
-    
+
     ## Define datasets...
     X1 <- norm_data(target_set[["X"]]);      y1 <- target_set[["y"]]
     X2 <- norm_data(source_set[["X"]]);      y2 <- source_set[["y"]]
     n_feat <- ncol(X1);                      data_lims <- c(0, 1)
-    
-    
+
+
     ## Distribution matching for predictors...
-    X2_map <- lapply(1:n_feat, function(j) { 
+    X2_map <- lapply(1:n_feat, function(j) {
         dist_match(X1[, j], ref = X2[, j], match_method = method, samp_size = size, lims = data_lims, rand_seed = seed)
         })
     X2_map <- as.data.frame(X2_map);    dimnames(X2_map) <- dimnames(X1)
     # rownames(X2_map) <- rownames(X1);   colnames(X2_map) <- colnames(X1)
-    
-    
+
+
     ## Perform prediction & map back to original space...
-    y2_pred_map <- RF_predict(x_train = X2, y_train = y2, x_test = X2_map, y_lims = data_lims, 
+    y2_pred_map <- RF_predict(x_train = X2, y_train = y2, x_test = X2_map, y_lims = data_lims,
                               n_tree = 200, m_try = 0.4, random_seed = seed)
     y2_cdf  <- get_dist_est(y2, sample_size = size, x_range = "unit", dist_method = method, grid_size = 1e3, random_seed = seed)
-    
-    y1_pred <- dist_match(y2_pred_map, ref = y1, src_dist = y2_cdf, match_method = method, samp_size = size, 
+
+    y1_pred <- dist_match(y2_pred_map, ref = y1, src_dist = y2_cdf, match_method = method, samp_size = size,
                           lims = data_lims, rand_seed = seed)
     y1_pred <- confined(y1_pred, lims = data_lims);      names(y1_pred) <- names(y2_pred_map)
-    
-    
+
+
     ## Return output objects...
     if (pred_all) {
         return( list("mapped" = y1_pred, "unmapped" = y2_pred_map) )
